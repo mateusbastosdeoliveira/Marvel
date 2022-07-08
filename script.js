e932c8ccc81f15b152c6123a38d941ef99a0bd56
Mateus Bastos <mateusbastosoliveira1920@gmail.com>
2022-07-08 20:33:50 -0300

    Update Marvel

---
+++ script.js
@@ -0,0 +1,107 @@
+
+        function trocaprohulk() {
+
+            document.getElementById("div-foto-subs").classList.add("vai-hulk")
+            document.getElementById("div-foto-subs").classList.remove("vai-spider")
+            document.getElementById("div-foto-subs").classList.remove("vai-arqueiro")
+            document.getElementById("div-foto-subs").classList.remove("vai-escarlate")
+            document.getElementById("div-foto-subs").classList.remove("vai-formiga")
+            document.getElementById("div-foto-subs").classList.remove("vai-loki")
+            document.getElementById("div-foto-subs").classList.remove("vai-mercurio")
+            document.getElementById("div-foto-subs").classList.remove("vai-venom")
+            
+
+
+        }
+
+
+        function trocaprospider() {
+            document.getElementById("div-foto-subs").classList.remove("vai-hulk")
+            document.getElementById("div-foto-subs").classList.add("vai-spider")
+            document.getElementById("div-foto-subs").classList.remove("vai-arqueiro")
+            document.getElementById("div-foto-subs").classList.remove("vai-escarlate")
+            document.getElementById("div-foto-subs").classList.remove("vai-formiga")
+            document.getElementById("div-foto-subs").classList.remove("vai-loki")
+            document.getElementById("div-foto-subs").classList.remove("vai-mercurio")
+            document.getElementById("div-foto-subs").classList.remove("vai-venom")
+            
+
+        }
+
+        function trocaproarqueiro() {
+            document.getElementById("div-foto-subs").classList.remove("vai-hulk")
+            document.getElementById("div-foto-subs").classList.remove("vai-spider")
+            document.getElementById("div-foto-subs").classList.add("vai-arqueiro")
+            document.getElementById("div-foto-subs").classList.remove("vai-escarlate")
+            document.getElementById("div-foto-subs").classList.remove("vai-formiga")
+            document.getElementById("div-foto-subs").classList.remove("vai-loki")
+            document.getElementById("div-foto-subs").classList.remove("vai-mercurio")
+            document.getElementById("div-foto-subs").classList.remove("vai-venom")
+            
+
+        }
+        function trocapraescarlate() {
+            document.getElementById("div-foto-subs").classList.remove("vai-hulk")
+            document.getElementById("div-foto-subs").classList.remove("vai-spider")
+            document.getElementById("div-foto-subs").classList.remove("vai-arqueiro")
+            document.getElementById("div-foto-subs").classList.add("vai-escarlate")
+            document.getElementById("div-foto-subs").classList.remove("vai-formiga")
+            document.getElementById("div-foto-subs").classList.remove("vai-loki")
+            document.getElementById("div-foto-subs").classList.remove("vai-mercurio")
+            document.getElementById("div-foto-subs").classList.remove("vai-venom")
+            
+
+        }
+
+        function trocaproformiga() {
+            document.getElementById("div-foto-subs").classList.remove("vai-hulk")
+            document.getElementById("div-foto-subs").classList.remove("vai-spider")
+            document.getElementById("div-foto-subs").classList.remove("vai-arqueiro")
+            document.getElementById("div-foto-subs").classList.remove("vai-escarlate")
+            document.getElementById("div-foto-subs").classList.add("vai-formiga")
+            document.getElementById("div-foto-subs").classList.remove("vai-loki")
+            document.getElementById("div-foto-subs").classList.remove("vai-mercurio")
+            document.getElementById("div-foto-subs").classList.remove("vai-venom")
+            
+
+        }
+
+        function trocaproloki() {
+            document.getElementById("div-foto-subs").classList.remove("vai-hulk")
+            document.getElementById("div-foto-subs").classList.remove("vai-spider")
+            document.getElementById("div-foto-subs").classList.remove("vai-arqueiro")
+            document.getElementById("div-foto-subs").classList.remove("vai-escarlate")
+            document.getElementById("div-foto-subs").classList.remove("vai-formiga")
+            document.getElementById("div-foto-subs").classList.add("vai-loki")
+            document.getElementById("div-foto-subs").classList.remove("vai-mercurio")
+            document.getElementById("div-foto-subs").classList.remove("vai-venom")
+            
+
+        }
+
+        function trocapromercurio() {
+            document.getElementById("div-foto-subs").classList.remove("vai-hulk")
+            document.getElementById("div-foto-subs").classList.remove("vai-spider")
+            document.getElementById("div-foto-subs").classList.remove("vai-arqueiro")
+            document.getElementById("div-foto-subs").classList.remove("vai-escarlate")
+            document.getElementById("div-foto-subs").classList.remove("vai-formiga")
+            document.getElementById("div-foto-subs").classList.remove("vai-loki")
+            document.getElementById("div-foto-subs").classList.add("vai-mercurio")
+            document.getElementById("div-foto-subs").classList.remove("vai-venom")
+            
+
+        }
+
+
+        function trocaprovenom() {
+            document.getElementById("div-foto-subs").classList.remove("vai-hulk")
+            document.getElementById("div-foto-subs").classList.remove("vai-spider")
+            document.getElementById("div-foto-subs").classList.remove("vai-arqueiro")
+            document.getElementById("div-foto-subs").classList.remove("vai-escarlate")
+            document.getElementById("div-foto-subs").classList.remove("vai-formiga")
+            document.getElementById("div-foto-subs").classList.remove("vai-loki")
+            document.getElementById("div-foto-subs").classList.remove("vai-mercurio")
+            document.getElementById("div-foto-subs").classList.add("vai-venom")
+            
+
+        }
